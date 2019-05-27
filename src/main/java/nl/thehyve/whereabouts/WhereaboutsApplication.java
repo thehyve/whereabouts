9ca71554c0c2864fae240567aa0d9772ad453b5a
ewelinagr <ewelina@thehyve.nl>
2019-05-27 16:43:04 +0200

    Add an empty Spring Boot app

---
+++ src/main/java/nl/thehyve/whereabouts/WhereaboutsApplication.java
@@ -0,0 +1,13 @@
+package nl.thehyve.whereabouts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereaboutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereaboutsApplication.class, args);
+	}
+
+}
