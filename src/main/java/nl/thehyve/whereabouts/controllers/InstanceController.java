c1bbea53d597d27984bcff973f223e3c1860fd74
ewelinagr <ewelina@thehyve.nl>
2019-06-04 13:37:52 +0200

    Change response status for POST '/instances', update docs

--- src/main/java/nl/thehyve/whereabouts/controllers/InstanceController.java
+++ src/main/java/nl/thehyve/whereabouts/controllers/InstanceController.java
@@ -7,12 +7,14 @@ package nl.thehyve.whereabouts.controllers;
 
 import nl.thehyve.whereabouts.dto.InstanceRepresentation;
 import nl.thehyve.whereabouts.services.InstanceService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 
@@ -36,6 +38,7 @@ public class InstanceController {
     }
 
     @PostMapping("/instances")
+    @ResponseStatus(HttpStatus.CREATED)
     public InstanceRepresentation addInstance(@RequestBody InstanceRepresentation newInstance) {
         return instanceService.addInstance(newInstance);
     }
