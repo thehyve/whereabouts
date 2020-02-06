565a4e3eb3b550ea0aad22c40870d634f9cd7d4c
Gijs Kant <gijs@thehyve.nl>
2020-02-06 21:19:22 +0100

    Enable Slack notifications.

---
+++ .travis/notify.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+message="<${TRAVIS_BUILD_WEB_URL}|Build #${TRAVIS_BUILD_NUMBER}> succeeded, application deployed. <https://github.com/${TRAVIS_REPO_SLUG}/tree/${TRAVIS_BRANCH}|Branch ${TRAVIS_BRANCH} on ${TRAVIS_REPO_SLUG}>, commit ${TRAVIS_COMMIT}: ${TRAVIS_COMMIT_MESSAGE}"
+curl -f -q -X POST --data-urlencode "payload={\"channel\": \"#dw-builds\", \"username\": \"whereabouts\", \"text\": \"${message}\"}" "${SLACK_WEBHOOK}"
