98740cde48cf112b9a967aec5a3c8b96b63daa03
Gijs Kant <gijs@thehyve.nl>
2020-07-03 10:30:30 +0200

    Update dependencies.

--- src/main/java/nl/thehyve/whereabouts/config/KeycloakConfig.java
+++ src/main/java/nl/thehyve/whereabouts/config/KeycloakConfig.java
@@ -1,15 +1,37 @@
 package nl.thehyve.whereabouts.config;
 
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
-import org.springframework.context.annotation.Bean;
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.KeycloakDeployment;
+import org.keycloak.adapters.KeycloakDeploymentBuilder;
+import org.keycloak.adapters.spi.HttpFacade;
+import org.keycloak.representations.adapters.config.AdapterConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class KeycloakConfig {
 
-    @Bean
-    public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
-        return new KeycloakSpringBootConfigResolver();
+    @Primary
+    @Configuration
+    static
+    class CustomKeycloakSpringBootConfigResolver implements KeycloakConfigResolver {
+
+        private final AdapterConfig adapterConfig;
+        private KeycloakDeployment keycloakDeployment;
+
+        @Autowired
+        CustomKeycloakSpringBootConfigResolver(AdapterConfig adapterConfig) {
+            this.adapterConfig = adapterConfig;
+        }
+
+        @Override
+        public KeycloakDeployment resolve(HttpFacade.Request request) {
+            if (this.keycloakDeployment == null) {
+                this.keycloakDeployment = KeycloakDeploymentBuilder.build(this.adapterConfig);
+            }
+            return this.keycloakDeployment;
+        }
     }
 
 }
